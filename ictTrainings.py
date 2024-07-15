c5e22496a58d4e554cb887d03420c57d3368ff84
LENOVO <samueloluwapelumi8@gmail.com>
2024-07-15 23:01:11 +0100

    first commit

--- ictTrainings.py
+++ ictTrainings.py
@@ -3,7 +3,14 @@ import streamlit as st
 st.title("ICT Trainings")
 
 st.image("galleryImages/codingSchool.png", caption="An Ongoing Registration")
-st.link_button("Register For This Bootcamp Now", "https://forms.gle/u9DRjkjA7z9aXtu3A")
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.info("Both the Registration and the Training cost =N= 10,000 only. Click on the first Button to make payment, "
+            "thereafter you will automatically be redirected to the Registration form")
+
+with col2:
+    st.link_button("Register For This Bootcamp At Once", "https://paystack.com/pay/coding-bootcamp")
+    st.link_button("Make further Enquiries before I pay", "https://wa.me/+2347077705842")
 st.divider()
 
 col1, col2 = st.columns(2, gap="medium", vertical_alignment="center")
